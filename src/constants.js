8feeebdc91fbca4ff4bc1ffba876c40d20d45ff7
David Däster <david.daester@unic.com>
2020-10-25 20:56:44 +0100

    #19 - Fix missing edges on richtext content type
    Enhance the relation extraction to be able to solve richtext elements.
    
    For simplification (and because this may be the normal use case), the extraction does not check limitation in size (min / max) of a specific reference configuration.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,11 @@
 const TYPE_LINK = 'Link';
 const LINK_TYPE_ASSET = 'Asset';
 const LINK_TYPE_ENTRY = 'Entry';
+const TYPE_RICH_TEXT = 'RichText';
+const RICH_TEXT_ENTRY_LINK_TYPES = ['entry-hyperlink', 'embedded-entry-block', 'embedded-entry-inline'];
 
 module.exports.TYPE_LINK = TYPE_LINK;
+module.exports.TYPE_RICH_TEXT = TYPE_RICH_TEXT;
+module.exports.RICH_TEXT_ENTRY_LINK_TYPES = RICH_TEXT_ENTRY_LINK_TYPES;
 module.exports.LINK_TYPE_ASSET = LINK_TYPE_ASSET;
 module.exports.LINK_TYPE_ENTRY = LINK_TYPE_ENTRY;
